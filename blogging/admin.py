e531ccb54b05ce691cc2e4e60b8caa231c7f3100
calvin fannin <calvin.fannin@gmail.com>
2020-05-09 18:16:50 -0700

    assignment 7

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author']
+    ordering = ['title']
+    inlines = [CategoryInline, ]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
